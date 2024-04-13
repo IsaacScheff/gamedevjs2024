34d0fd6bb958fff3cc256c08538bd908932e20f6
Isaac Scheff <isaac1996@comcast.net>
2024-04-13 12:38:29 -0400

    Initial commit

---
+++ src/game.js
@@ -0,0 +1,31 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+    // Load assets
+}
+
+function create() {
+    // Create game entities
+}
+
+function update() {
+    // Game logic for each frame
+}
